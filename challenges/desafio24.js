2b66a04ec49c017bf982e8962672d1d6379b481f
Karin Melo <karinsantosmelo@gmail.com>
2023-04-06 02:26:06 -0300

    Desafios 1 ao 29 correção lint 2

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({},
-    { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } },
-);
+    { $push: { valoresNutricionais: { $each: [], $sort: { percentual: -1 } } } });
+    
 db.produtos.find({}, { valoresNutricionais: true, nome: true, _id: false });
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,3 +1,5 @@
 db.produtos.find(
-    { $and: [{ vendidos: { $ne: 50 } }, { tags: { $exists: 0 } }] }, { nome: 1, _id: 0, vendidos: 1 },
+    { 
+        $and: [{ vendidos: { $ne: 50 } }, { tags: { $exists: 0 } }], 
+    }, { nome: 1, _id: 0, vendidos: 1 },
 );
